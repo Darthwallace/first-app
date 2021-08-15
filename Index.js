a5e124e12315cee837658460da48ee82f6fbc46c
Wallace <54440323+Darthwallace@users.noreply.github.com>
2021-08-15 13:53:19 -0300

    Update Index.js
    Codigo atualizado no github

--- Index.js
+++ Index.js
@@ -1,11 +1,21 @@
 const express = require("express"); 
 const app = express(); 
-const port=3000; 
+const routes =require("./routes");
+const expressLayouts = require("express-ejs-layouts");
+
+const port=3030; 
 const address="localhost"; 
+
+
+app.set('view engine','ejs');
+app.use(expressLayouts);
+
+
+app.use('/',routes);
+
 //Criando um servidor simples com o Node.js e o Express
 const server = app.listen(port,address,()=>{ 
  let host = server.address().address; 
  let port = server.address().port; 
- console.log(`Servidor executando no endereço ${host} e porta $
-{port}`); 
+ console.log(`Servidor executando no endereço ${host} e porta ${port}`); 
 })
