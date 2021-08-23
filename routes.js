68c83f9a1c5fce32052878ea8c279db1317d6d8f
Darthwallace <wallaceleite915@gmail.com>
2021-08-23 19:52:45 -0300

    atualizando

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const app =express();
+//const app =express();
 //const expressLayouts = require("express-ejs-layouts");
 //const faker = require("faker");
 
@@ -14,54 +14,102 @@ router.use(express.static('public'));
 
 router.get('/',(req,res)=>{
     res.render('pages/home');
-    //res.send('pagina de teste');
+    
 });
 
-module.exports = router;
+// router.get('/',(req,res)=>{ 
+//  res.render('pages/home'); 
+// }); 
 
-router.use(express.static('public'));
-router.get('/',(req,res)=>{ 
- res.render('pages/home'); 
-}); 
-router.get('/about',(req,res)=>{ 
- //let usuarios=[]; 
- //Usando o Faker para criar 6 perfis para colocar no about
- //for(let cont=1;cont<=6;cont++){ 
- //usuarios.push({name:faker.name.findName(),email:
-//faker.internet.email(),avatar: faker.image.image()}); 
- //} 
- //console.log(usuarios); 
+router.get('/about',(req,res)=>{  
  res.render('pages/about'); 
 }); 
 
 router.get('/cadastro',(req,res)=>{
-    let usuarios = [{name:"wallace",endereco:"Rua bom jesus, 2641",email:"wallaceleite915@gmail.com",age:22,height:1.80,vote:true},
-                    {name:"wesley",endereco:"Rua bom jesus, 2655",email:"wesley915@gmail.com",age:15,height:1.75,vote:false}];    
-    res.render('pages/cadastro',{usuarios});
+    res.render('pages/cadastro',{usuarios:usuarios});
+});
+
+router.post('/cadastro/remove',(req,res)=>{
+        let name = req.body.name;
+
+        if(usuarios.length==0){
+            console.log("Erro: não há elemento a ser removido!");
+            return res.status(500).json({
+                status:'error',
+                error:'Removed element: ${name}'
+            });
+        } else {
+            for(let cont=0;cont<usuarios.length;cont++){
+                if(usuarios[cont].name==name){
+                    usuarios.splice(cont,1);
+                    console.log("Elemento Removido: ",name);
+                    return res.status(200).json({
+                        status:'sucess',
+                        data:usuarios
+                    });
+                    //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
+                } else if(cont==usuarios.length-1){
+                    console.log("Erro ao remover elemento: ",name);
+                    return res.status(400).json({
+                        status:'error',
+                        error:`Removed element: ${name}`
+                    });
+                }
+            }
+        }
+    
+    
+    
+    //res.send('remoção realizada com sucesso');
+   });
+
+
+
+
+router.post('/cadastro/update',(req,res)=>{
+    usuarios[req.body.id].name=req.body.name;
+    usuarios[req.body.id].endereco=req.body.endereco;
+    usuarios[req.body.id].email=req.body.email;
+    usuarios[req.body.id].age=req.body.age;
+    usuarios[req.body.id].height=req.body.height;
+    usuarios[req.body.id].vote=req.body.vote;    
+    
+
+    res.sendStatus(200);
+    console.log("Dados recebidos: ",req.body);
+    //res.send('atualização realizada com sucesso');
+   });
+
+
+router.get('/cadastro/list',(req,res)=>{
+
+});
+//router.get('/cadastro/insert',(req,res)=>{
+//});
+router.post('/cadastro/add',(req,res)=>{
+    let usuario={name:"",email:"",endereco:"",heigth:"",age:"",vote:""};
+
+    usuario.name = req.body._name;
+    usuario.email = req.body._email;
+    usuario.endereco = req.body._endereco;
+    usuario.heigth = req.body._heigth;
+    usuario.age = req.body._age;
+    usuario.vote = req.body._vote;
+
+    usuario.push(usuario);
+    console.log("Usuário cadastrado: ",usuario);
+    console.log("Lista dos usuários: ",usuarios)
+    res.sendStatus(200);
+    res.status(200).json({
+        status:'sucess',
+        data: 'Usuário ${usuario} foi adicionado com sucesso!'
     });
-
-router.get('/cadastro/remove/:id',(req,res)=>{
-        res.send('remoção realizada com sucesso');
-   });
-
-   router.get('/cadastro/update/:id',(req,res)=>{
-        res.send('atualização realizada com sucesso');
-   });
-
-router.get('/cadastro/insert',(req,res)=>{
 });
 
-
-router.get('/curriculo',(req,res)=>{ 
- res.send('Meu currículo'); 
-});
-
-   router.get('/cadastro/list',(req,res)=>{
-   //listar de usuarios cadastrado 
-   });
+//router.get('/curriculo',(req,res)=>{ 
+ //res.send('Meu currículo'); 
+//});
+module.exports = router;
    //Essa linha permite que este código seja exportado como um 
-   //módulo e possa ser usado em outras partes da aplicação. 
-   module.exports = router; 
 
 
-   
